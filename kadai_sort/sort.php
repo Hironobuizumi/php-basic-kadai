52c1e18c38a1d8f981344750732ff36e7d38cee4
Hironobuizumi <158254076+Hironobuizumi@users.noreply.github.com>
2024-05-20 11:46:50 +0900

    sort

---
+++ kadai_sort/sort.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        function sort_2way(&$array, $order) {
+            if ($order === true) {
+                
+                sort($array);
+            } elseif ($order === false) {
+                
+                rsort($array);
+            }
+        }
+        
+        
+        $nums = [15, 4, 18, 23, 10];
+        
+        
+        sort_2way($nums, true);
+        echo "昇順にソートします。<br>";
+        foreach ($nums as $num) {
+            echo $num . '<br>';
+        }
+        
+        
+        $nums = [15, 4, 18, 23, 10];
+        
+        
+        sort_2way($nums, false);
+        echo "降順にソートします。<br>";
+        foreach ($nums as $num) {
+            echo $num . '<br>';
+        }
+        
+        ?>
+    </p>
+</body>
+
+</html>
